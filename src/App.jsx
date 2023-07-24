3c02cc458830dfc50e53ceeb7f4baf0051ae5dfa
fabianc@uazuay.edu.ec <fabianc@uazuay.edu.ec>
2023-07-24 12:08:48 -0500

    tercero

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 // App.js
 import React, { useState, useEffect } from 'react';
-import { v4 as uuidv4 } from 'uuid';
+
 import './App.css'; // Archivo CSS para estilos personalizados
 
 const App = () => {
